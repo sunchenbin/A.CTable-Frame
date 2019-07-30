c5021ef76b15bb0d518b0d9cdc4d8b6f142838dc
chenbin.sun <SSH4557@windows10.microdone.cn>
2019-07-30 12:21:17 +0800

    issues/IYW9F:mybatis.model.pack支持多包扫描","或者";"隔开

--- mybatis-enhance-actable/src/main/java/com/gitee/sunchenbin/mybatis/actable/utils/ClassTools.java
+++ mybatis-enhance-actable/src/main/java/com/gitee/sunchenbin/mybatis/actable/utils/ClassTools.java
@@ -27,91 +27,94 @@ public class ClassTools{
 	/**
 	 * 从包package中获取所有的Class
 	 * 
-	 * @param pack 扫描的包
+	 * @param packs 扫描的包，支持多个","或者";"隔开
 	 * @return 该包下的class
 	 */
-	public static Set<Class<?>> getClasses(String pack){
-
+	public static Set<Class<?>> getClasses(String packs){
+		
 		// 第一个class类的集合
 		Set<Class<?>> classes = new LinkedHashSet<Class<?>>();
-		// 是否循环迭代
-		boolean recursive = true;
-		// 获取包的名字 并进行替换
-		String packageName = pack;
-		String packageDirName = packageName.replace('.', '/');
-		// 定义一个枚举的集合 并进行循环来处理这个目录下的things
-		Enumeration<URL> dirs;
-		try{
-			dirs = Thread.currentThread().getContextClassLoader().getResources(packageDirName);
-			// 循环迭代下去
-			while (dirs.hasMoreElements()){
-				// 获取下一个元素
-				URL url = dirs.nextElement();
-				// 得到协议的名称
-				String protocol = url.getProtocol();
-				// 如果是以文件的形式保存在服务器上
-				if ("file".equals(protocol)) {
-					System.err.println("file类型的扫描");
-					// 获取包的物理路径
-					String filePath = URLDecoder.decode(url.getFile(), "UTF-8");
-					// 以文件的方式扫描整个包下的文件 并添加到集合中
-					findAndAddClassesInPackageByFile(packageName, filePath, recursive, classes);
-				}else if ("jar".equals(protocol)) {
-					// 如果是jar包文件
-					// 定义一个JarFile
-					System.err.println("jar类型的扫描");
-					JarFile jar;
-					try{
-						// 获取jar
-						jar = ((JarURLConnection) url.openConnection()).getJarFile();
-						// 从此jar包 得到一个枚举类
-						Enumeration<JarEntry> entries = jar.entries();
-						// 同样的进行循环迭代
-						while (entries.hasMoreElements()){
-							// 获取jar里的一个实体 可以是目录 和一些jar包里的其他文件 如META-INF等文件
-							JarEntry entry = entries.nextElement();
-							String name = entry.getName();
-							// 如果是以/开头的
-							if (name.charAt(0) == '/') {
-								// 获取后面的字符串
-								name = name.substring(1);
-							}
-							// 如果前半部分和定义的包名相同
-							if (name.startsWith(packageDirName)) {
-								int idx = name.lastIndexOf('/');
-								// 如果以"/"结尾 是一个包
-								if (idx != -1) {
-									// 获取包名 把"/"替换成"."
-									packageName = name.substring(0, idx).replace('/', '.');
+		// 拆成多个pack，支持多个
+		String[] split = packs.split(",|;");
+		for (String pack : split) {
+			// 是否循环迭代
+			boolean recursive = true;
+			// 获取包的名字 并进行替换
+			String packageName = pack;
+			String packageDirName = packageName.replace('.', '/');
+			// 定义一个枚举的集合 并进行循环来处理这个目录下的things
+			Enumeration<URL> dirs;
+			try{
+				dirs = Thread.currentThread().getContextClassLoader().getResources(packageDirName);
+				// 循环迭代下去
+				while (dirs.hasMoreElements()){
+					// 获取下一个元素
+					URL url = dirs.nextElement();
+					// 得到协议的名称
+					String protocol = url.getProtocol();
+					// 如果是以文件的形式保存在服务器上
+					if ("file".equals(protocol)) {
+						System.err.println("file类型的扫描:" + pack);
+						// 获取包的物理路径
+						String filePath = URLDecoder.decode(url.getFile(), "UTF-8");
+						// 以文件的方式扫描整个包下的文件 并添加到集合中
+						findAndAddClassesInPackageByFile(packageName, filePath, recursive, classes);
+					}else if ("jar".equals(protocol)) {
+						// 如果是jar包文件
+						// 定义一个JarFile
+						System.err.println("jar类型的扫描");
+						JarFile jar;
+						try{
+							// 获取jar
+							jar = ((JarURLConnection) url.openConnection()).getJarFile();
+							// 从此jar包 得到一个枚举类
+							Enumeration<JarEntry> entries = jar.entries();
+							// 同样的进行循环迭代
+							while (entries.hasMoreElements()){
+								// 获取jar里的一个实体 可以是目录 和一些jar包里的其他文件 如META-INF等文件
+								JarEntry entry = entries.nextElement();
+								String name = entry.getName();
+								// 如果是以/开头的
+								if (name.charAt(0) == '/') {
+									// 获取后面的字符串
+									name = name.substring(1);
 								}
-								// 如果可以迭代下去 并且是一个包
-								if ((idx != -1) || recursive) {
-									// 如果是一个.class文件 而且不是目录
-									if (name.endsWith(".class") && !entry.isDirectory()) {
-										// 去掉后面的".class" 获取真正的类名
-										String className = name.substring(packageName.length() + 1, name.length() - 6);
-										try{
-											// 添加到classes
-											classes.add(Class.forName(packageName + '.' + className));
-										}catch (ClassNotFoundException e){
-											// log
-											// .error("添加用户自定义视图类错误 找不到此类的.class文件");
-											e.printStackTrace();
+								// 如果前半部分和定义的包名相同
+								if (name.startsWith(packageDirName)) {
+									int idx = name.lastIndexOf('/');
+									// 如果以"/"结尾 是一个包
+									if (idx != -1) {
+										// 获取包名 把"/"替换成"."
+										packageName = name.substring(0, idx).replace('/', '.');
+									}
+									// 如果可以迭代下去 并且是一个包
+									if ((idx != -1) || recursive) {
+										// 如果是一个.class文件 而且不是目录
+										if (name.endsWith(".class") && !entry.isDirectory()) {
+											// 去掉后面的".class" 获取真正的类名
+											String className = name.substring(packageName.length() + 1, name.length() - 6);
+											try{
+												// 添加到classes
+												classes.add(Class.forName(packageName + '.' + className));
+											}catch (ClassNotFoundException e){
+												// log
+												// .error("添加用户自定义视图类错误 找不到此类的.class文件");
+												e.printStackTrace();
+											}
 										}
 									}
 								}
 							}
+						}catch (IOException e){
+							// log.error("在扫描用户定义视图时从jar包获取文件出错");
+							e.printStackTrace();
 						}
-					}catch (IOException e){
-						// log.error("在扫描用户定义视图时从jar包获取文件出错");
-						e.printStackTrace();
 					}
 				}
+			}catch (IOException e){
+				e.printStackTrace();
 			}
-		}catch (IOException e){
-			e.printStackTrace();
 		}
-
 		return classes;
 	}
 
